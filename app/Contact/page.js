185809bcf91e3470d881b9d6cc0ab04af868330d
Harsh Jain <112223901+harshj2109@users.noreply.github.com>
2023-10-10 00:00:19 +0530

    Add files via upload

---
+++ app/Contact/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React, { useEffect } from 'react'
+import Image from 'next/image'
+import logo from './public/puerim.png'
+import Link from 'next/link'
+
+const contact = () => {
+  return (
+    <>
+    <h1 data className="head">hello</h1>
+    </>
+  )
+}
+
+export default contact
